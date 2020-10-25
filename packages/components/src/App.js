70a85c9d4b5fe975a7c048f8cef767030de8ea57
Oleksandr Kurinnyi <exzos@MacBook-Pro-Oleksandr.local>
2020-10-25 22:42:47 +0100

    initial commit

---
+++ packages/components/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Monorepo</Text>
+    </View>
+  );
+};
+
+
+export default App;
